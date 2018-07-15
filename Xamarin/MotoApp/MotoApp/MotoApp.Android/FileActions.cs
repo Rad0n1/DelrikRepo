82372b66f56a6334f33ffb69f16947e86380c653
Unknown <chemicalfactory4@gmail.com>
2018-07-15 05:35:49 +0200

    motorApp Added
    Backup before pc resetting

---
+++ Xamarin/MotoApp/MotoApp/MotoApp.Android/FileActions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PCLStorage;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MotoApp.Droid
+{
+    class FileActions
+    {
+        public async Task WriteFile(string folderName, string fileName, string[] decodedData)
+        {
+            string oldData = string.Empty;
+            string encodedData = await EncodeData(decodedData);
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            IFolder saveFolder = await rootFolder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
+
+            ExistenceCheckResult checkResult = await saveFolder.CheckExistsAsync(fileName);
+            if (checkResult == ExistenceCheckResult.NotFound)
+            {
+                IFile newData = await saveFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+                await newData.WriteAllTextAsync(encodedData);
+            }
+            else
+            {
+                IFile oldDataFile = await saveFolder.GetFileAsync(fileName);
+                oldData = await oldDataFile.ReadAllTextAsync();
+                oldData += encodedData;
+                IFile newData = await saveFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+                await newData.WriteAllTextAsync(oldData);
+            }
+
+
+        }
+
+        public async Task<string[]> GetFileData(string folderName, string fileName)
+        {
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            IFolder saveFolder = await rootFolder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
+            IFile saveData = await saveFolder.GetFileAsync(fileName);
+            string encodedData = await saveData.ReadAllTextAsync();
+            string[] decodedData = encodedData.Split('.');
+            return decodedData;
+        }
+
+        private async Task<string> EncodeData(string[] data)
+        {
+            string encodedData = string.Empty;
+            foreach (string item in data)
+            {
+                encodedData += item + ".";
+            }
+
+            return encodedData;
+        }
+    }
+}
---
+++ Xamarin/MotoApp/MotoApp/MotoApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using PCLStorage;
+using MotoApp.Droid;
+
+namespace MotoApp
+{
+	public partial class MainPage : ContentPage
+	{
+        FileActions fileActions = new FileActions();
+        string folderName = "saveDataFolder";
+        string fileName = "saveData.txt";
+
+        public MainPage()
+		{
+
+            InitializeComponent();
+            this.FindByName<Button>("writeButton").Clicked += WriteFile;
+            this.FindByName<Button>("readButton").Clicked += ReadFile;
+		}
+
+        private async void WriteFile(object sender, EventArgs e)
+        {
+            string[] data;
+            string stringData = value1.Text + "." + value2.Text + "." + value3.Text + ".";
+            data = stringData.Split('.');
+
+            await fileActions.WriteFile(folderName, fileName, data);
+        }
+
+        private async void ReadFile(object sender, EventArgs e)
+        {
+            string[] fileData = await fileActions.GetFileData(folderName, fileName);
+            foreach (var item in fileData)
+            {
+                label1.Text += item;
+            }
+        }
+	}
+}
